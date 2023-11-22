4ad53c84f13f34b54840790c1b06e70aa1e1d2d1
Paula Abib <104170184+jed1rey@users.noreply.github.com>
2023-11-22 17:53:06 -0300

    Add files via upload
    modificacao logout

--- logado.php
+++ logado.php
@@ -28,30 +28,31 @@
 
 <body>
 
-    <!-- Logo + Menu de Navegação -->
+<!–---------Logo + Menu de Navegação ------------–>
+
     <header class="header">
-        <a href="index.html"><img src="img/logo.png" class="logo" /></a>
-        <input class="menu-btn" type="checkbox" id="menu-btn" />
-        <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-        <ul class="menu">
-            <li><a href="index.html"><i class="fa fa-home-alt"></i> Home</a></li>
-            <li><a href="especialidades.php"><i class="fa fa-stethoscope"></i> Especialidades</a></li>
-            <li><a href="planos.html"><i class="fas fa-laptop-medical"></i> Planos</a></li>
-            <li><a href="sobre.html"><i class="fas fa-vector-square"></i> Sobre Nós</a></li>
-          </ul>
+      <a href="index.html"><img src="img/logo.png" class="logo" /></a>
+      <input class="menu-btn" type="checkbox" id="menu-btn" />
+      <label class="menu-icon" for="menu-btn"
+        ><span class="navicon"></span></label>
+      <ul class="menu">
+        <li><a href="index.html"><i class="fa fa-home-alt"></i> Home</a></li>
+        <li><a href="especialidades.php"><i class="fa fa-stethoscope"></i> Especialidades</a></li>
+        <li><a href="planos.html"><i class="fas fa-laptop-medical"></i> Planos</a></li>
+        <li><a href="sobre.html"><i class="fas fa-vector-square"></i> Sobre Nós</a></li>
+        <?php if (isset($_SESSION['nomeUser'])) : ?>
+                <li><a href="logout.php"><i class="fa fa-sign-out-alt"></i> Logout</a></li>
+            <?php endif; ?>
+        </li>
+      </ul>
     </header>
 
-    <br><br>
+<br><br><br><br>
 
     <!-- Início da página pacientes -->
     <main>
         <?php if (isset($_SESSION['nomeUser'])) : ?>
             <div class="profile-container">
-                <div class="profile">
-                    <img src="SQL fotoUser" alt="Foto de perfil">
-                </div>
-                $pastaUploads = 'caminho/para/a/pasta/uploads/';
-
                 <!-- Botões -->
                 <div class="section">
                     <div class="absolute">
@@ -84,16 +85,20 @@
                                 <?php echo $_SESSION['tipoSanguineoFromDB']; ?>
                             </span><br></p>
                         <br><br>
-                        <h3>Medicamentos Utilizados</h3>
-                        <p>
-                            <?php echo $_SESSION['qualMedicamentoFromDB']; ?>
-                        </p>
+                        <p><br><span class="titulo">Qual Medicamento:</span><br><span class="conteudo">
+                                <?php echo $_SESSION['qualMedicamentoFromDB']; ?>
+                            </span><br></p>
                     </div>
                 </div>
             </div>
         <?php else : ?>
             <p>Sessão não iniciada ou informações não disponíveis.</p>
         <?php endif; ?>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
     </main>
 
     <script>
@@ -116,10 +121,80 @@
         }
     </script>
 
-    <!-- RODAPÉ -->
-    <div class="footer">
-        <!-- Conteúdo do rodapé -->
+ <!–---------RODAPE ------------–>
+
+  <div class="footer">
+    <div class="row main">
+      <div class="col site-info">
+        <div class="footer-disclaimer">
+          <div class="logo">
+            <img src="./img/logo.png" alt="" />
+          </div>
+         <p>A DoctorLink é uma plataforma de telemedicina acessível e segura. </p>
+        </div>
+        <div class="footer-links" id="footer">
+          <ul>
+            <li><a href="index.html" alt=""><i class="fa fa-home-alt"></i> Home</a></li>
+            <li><a href="especialidades.html" alt=""><i class="fa fa-stethoscope"></i> Especialidades</a></li>
+            <li><a href="planos.html" alt=""><i class="fas fa-laptop-medical"></i> Planos</a></li>
+            <li><a href="sobre.html" alt=""><i class="fas fa-vector-square"></i> Sobre Nós</a></li>
+            
+          </ul>
+        </div>
+      </div>
+     
+
+      <div class="col">
+        <div class="footer-links">
+          <p><strong>Especialidades</strong></p>
+          <ul style="margin-left: 40px;">
+            <li><a href="especialidades.php"><i class="fa fa-user-doctor"></i> Angiologia</a></li>
+            <li><a href="especialidades.php"><i class="fa-brands fa-mendeley"></i> Assitência Pré Natal</a></li>
+            <li><a href="especialidades.php"><i class="fa fa-heart"></i> Cardiologia</a></li> 
+            <li><a href="especialidades.php"><i class="fa fa-stethoscope"></i> Dermatologia</a></li> 
+            <li><a href="especialidades.php"><i class="fa fa-apple-whole"></i> Nutrição</a></li> 
+            <li><a href="especialidades.php"><i class="fa fa-brain"></i> Neurologia</a></li> 
+            <li><a href="especialidades.php"><i class="fa fa-spa"></i> Psicologia</a></li>
+            <li><a href="especialidades.php"><i class="fa fa-baby"></i> Pediatria</a></li>
+
+          </ul>
+          
+        </div>
+      </div>
+
+      <div class="col maps">
+        <div class="place">
+          <div class="map-container">
+            <div class="map"></div>
+          </div>
+          <div class="address">
+            <p><strong>Fale Conosco</strong></p>
+            <p>+55 016 3234-5678</p>
+            <p>email@doctorlink.com.br</p>
+          </div>
+        </div>
+        <a href="fale-conosco.html"><img src="./img/vector-01.jpg" alt="Fale Conosco"></a>
+      </div>
+
     </div>
+
+
+    <div class="row">
+      <div class="social-icons">
+        <ul >
+          <li><a href="#" target="_blank" alt=""><i class="fab fa-facebook"></i></a></li>
+          <li><a href="#" target="_blank" alt=""><i class="fab fa-twitter"></i></a></li>
+          <li><a href="#" target="_blank" alt=""><i class="fab fa-instagram"></i></a></li>
+          <li><a href="#" target="_blank" alt=""><i class="fab fa-youtube"></i></a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="ending-note">
+      <p>Copyright © 2023 DoctorLink. Todos os direitos reservados.</p>
+    </div>
+  </div>
+  <!–-------FIM RODAPE ------------–>
 
     <!-- Botão do WhatsApp -->
     <a class="wa" href="https://wa.me/5516992009026?text=Olá" target="_blank">
